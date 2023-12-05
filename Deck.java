645dcf24b99091a21f3565bcfd1ee75c5a3305bb
Thanhquan2601 <145652317+Thanhquan2601@users.noreply.github.com>
2023-12-05 16:43:25 +0700

    Deck.java

---
+++ Deck.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Deck {
+
+    private ArrayList<Card> deck;
+
+    public Deck(){
+        deck = new ArrayList<Card>();
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 13; j++){
+                if(j==0){
+                    Card card = new Card(i,j,11); //11 la ace
+                    deck.add(card);
+                }
+                else if(j >= 10){
+                    Card card = new Card(i,j, 10); // 10 la JQK
+                    deck.add(card);
+                }else{
+                    Card card = new Card(i,j,j+1);
+                    deck.add(card);
+                }
+            }
+        }
+    }
+
+        // xao bai
+        public void shuffleDeck() {
+            Collections.shuffle(deck);
+        }
+        public Card getCard(int i){
+            return deck.get(i);
+        }
+        public Card removeCard(int i){
+            return deck.remove(i);
+        }
+    }
